9527619aaa2077f1cd82ad401d8a060f2d2b1a9b
ANUJ KUMAR <kumar10305ak@gmail.com>
2026-07-09 00:18:50 +0530

    Time: 77 ms (47.11%), Space: 14.4 MB (61.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -25,38 +25,69 @@
 //     }
 // };
 
+//recursion + memoization 
+// class Solution {
+// public:
+//     int n;
+//     int t[2501][2501];
+
+//     int solve(vector<int>& nums, int i, int prev) {
+//         if (i >= n) {
+//             return 0;
+//         }
+//         if (prev != -1 && t[i][prev] != -1){
+//             return t[i][prev];
+//         }
+
+//         int take = 0;
+
+//         // when the previous element is smaller than current element then take otherwise skip
+//         if (prev == -1 || nums[prev] < nums[i]) {
+//             take = 1 + solve(nums, i + 1, i);
+//         }
+
+//         int skip = solve(nums, i + 1, prev);
+//         if (prev !=-1){
+//             t[i][prev] = max(take,skip);
+//         }
+
+//         return  max(take, skip);
+//     }
+
+//     int lengthOfLIS(vector<int>& nums) {
+//         n = nums.size();
+//         memset(t,-1,sizeof(t));
+//         return solve(nums, 0, -1);
+//     }
+// };
+
+
+//bottom up approach 
 
 class Solution {
 public:
-    int n;
-    int t[2501][2501];
-
-    int solve(vector<int>& nums, int i, int prev) {
-        if (i >= n) {
-            return 0;
-        }
-        if (prev != -1 && t[i][prev] != -1){
-            return t[i][prev];
-        }
-
-        int take = 0;
-
-        // when the previous element is smaller than current element then take otherwise skip
-        if (prev == -1 || nums[prev] < nums[i]) {
-            take = 1 + solve(nums, i + 1, i);
-        }
-
-        int skip = solve(nums, i + 1, prev);
-        if (prev !=-1){
-            t[i][prev] = max(take,skip);
-        }
-
-        return  max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(nums, 0, -1);
+        int n = nums.size();
+
+        vector<int> t(n, 1);
+
+        // t[i] = LIS ending at ith index
+
+        int maxLIS = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[j] < nums[i]) {
+
+                    t[i] = max(t[i], t[j] + 1);
+
+                    maxLIS = max(maxLIS, t[i]);
+                }
+            }
+        }
+
+        return maxLIS;
     }
 };
