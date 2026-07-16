e33da4690078f2a96194ba3135c6e254d79b6e49
ANUJ KUMAR <kumar10305ak@gmail.com>
2026-07-17 01:53:12 +0530

    Time: 398 ms (19.88%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+  id,
+  (CASE WHEN id % 2 = 1 
+        THEN COALESCE(LEAD(student) OVER(ORDER BY id),student)
+        ELSE LAG(student) OVER(ORDER BY id)
+        END) AS student
+FROM Seat
