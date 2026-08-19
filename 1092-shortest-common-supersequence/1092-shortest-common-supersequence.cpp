7f7d299a70cd04d00c268bd7271a47e8169bdd67
ANUJ KUMAR <kumar10305ak@gmail.com>
2026-08-19 16:35:59 +0530

    Time: 19 ms (97.51%), Space: 13.1 MB (97.81%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,63 +1,99 @@
 class Solution {
 public:
+
+    int dp[1001][1001];
+
+    int solve(int i, int j, string &str1, string &str2) {
+
+        // Base cases
+        if (i == 0)
+            return j;
+
+        if (j == 0)
+            return i;
+
+        // Already calculated
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        // Characters are same
+        if (str1[i - 1] == str2[j - 1]) {
+            return dp[i][j] =
+                1 + solve(i - 1, j - 1, str1, str2);
+        }
+
+        // Characters are different
+        return dp[i][j] =
+            1 + min(
+                solve(i - 1, j, str1, str2),
+                solve(i, j - 1, str1, str2)
+            );
+    }
+
+
     string shortestCommonSupersequence(string str1, string str2) {
+
         int m = str1.size();
         int n = str2.size();
-        vector<vector<int>> t(m+1,vector<int>(n+1));
 
-        for(int i = 0; i< m +1; i++){
-            for(int j = 0 ; j < n+1;j++){
-                if ( i == 0 || j == 0){
-                    t[i][j]= i+j;
-                }
-                else if (str1[i-1]== str2[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
-                }
-                else{
-                    t[i][j] = 1 + min(t[i-1][j],t[i][j-1]);
-                }
-            }
-        }
-        
-        // for printing shortest common sss
-        string res ="";
-        int i = m ; 
+        memset(dp, -1, sizeof(dp));
+
+        // First calculate SCS length
+        solve(m, n, str1, str2);
+
+        // Now construct the actual SCS
+        string res = "";
+
+        int i = m;
         int j = n;
-        while ( i> 0 && j > 0){
-            if (str1[i-1] == str2[j-1]){
-                res.push_back(str1[i-1]);
-                // then move diagonally 
+
+        while (i > 0 && j > 0) {
+
+            // Same character -> take it once
+            if (str1[i - 1] == str2[j - 1]) {
+
+                res.push_back(str1[i - 1]);
+
                 i--;
                 j--;
             }
-            else{
-                if (t[i-1][j]< t[i][j-1]){
-                    res.push_back(str1[i-1]);
+
+            // Different characters
+            else {
+
+                int option1 = solve(i - 1, j, str1, str2);
+                int option2 = solve(i, j - 1, str1, str2);
+
+                if (option1 < option2) {
+
+                    // Take character from str1
+                    res.push_back(str1[i - 1]);
                     i--;
-                }
-                else{
-                    res.push_back(str2[j-1]);
+
+                } else {
+
+                    // Take character from str2
+                    res.push_back(str2[j - 1]);
                     j--;
                 }
             }
-
         }
 
-        //now check if the i and j length end then 
-        while(i>0){
-            res.push_back(str1[i-1]);
+        // Remaining characters of str1
+        while (i > 0) {
+            res.push_back(str1[i - 1]);
             i--;
         }
-        while(j>0){
-            res.push_back(str2[j-1]);
+
+        // Remaining characters of str2
+        while (j > 0) {
+            res.push_back(str2[j - 1]);
             j--;
         }
 
-        //the result part in reverse so reverse to get the correct string
+        // We constructed it backwards
+        reverse(res.begin(), res.end());
 
-        reverse(res.begin(),res.end());
         return res;
-
-        
     }
 };
